003840b0af509f74805bcef85f435d3f9d253d97
Micke Prag <micke.prag@telldus.se>
2013-07-30 08:47:35 +0200

    Initial version of the loader for loading our plugins

---
+++ run.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import pkg_resources
+
+if __name__ == "__main__":
+	paths = ['plugins']
+
+	distributions, errors =  pkg_resources.working_set.find_plugins(pkg_resources.Environment(paths))
+	for dist in distributions:
+		if dist not in pkg_resources.working_set:
+			print("Loading plugin %s" % dist)
+			try:
+				pkg_resources.working_set.add(dist)
+			except Exception, e:
+				print("Could not load", dist, e)
+	for entry in pkg_resources.working_set.iter_entry_points('telldus.main'):
+		moduleClass = entry.load()
+		m = moduleClass()
+		# telldus.main is blocking. Only load one
+		break
