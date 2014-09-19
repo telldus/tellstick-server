196eed84f2bde316defaa0ba0f07c3f982ac8c0e
Stefan Persson <stefan.persson@telldus.se>
2014-09-19 11:21:52 +0200

    Fixes for Scheduler to be able to delete jobs, run on startup if within retry interval, etc

--- scheduler/src/scheduler/base/Scheduler.py
+++ scheduler/src/scheduler/base/Scheduler.py
@@ -17,10 +17,12 @@ class Scheduler(Plugin):
 
 	def __init__(self):
 		self.running = False
-		#self.runningJobsLock = threading.Lock()
+		#self.runningJobsLock = threading.Lock() #TODO needed?
+		self.jobsLock = threading.Lock()
 		self.runningJobs = {} #id:s as keys
 		self.s = Settings('telldus.scheduler')
 		Application().registerShutdown(self.stop)
+		self.jobs = []
 		self.fetchLocalJobs()
 		self.live = TelldusLive(self.context)
 		self.deviceManager = DeviceManager(self.context)
@@ -33,18 +35,22 @@ class Scheduler(Plugin):
 
 	def calculateJobs(self, jobs):
 		"""Calculate nextRunTime for all jobs in the supplied list, order it and assign it to self.jobs"""
+		newJobs = []
 		for job in jobs:
-			self.calculateNextRunTime(job)
+			self.checkNewlyLoadedJob(job)
+			if self.calculateNextRunTime(job):
+				newJobs.append(job)
 
-		jobs.sort(key=lambda job: job['nextRunTime'])
+		newJobs.sort(key=lambda job: job['nextRunTime'])
 		with self.jobsLock:
-			self.jobs = jobs
+			self.jobs = newJobs
 
 	def calculateNextRunTime(self, job):
 		"""Calculates nextRunTime for a job, depending on time, weekday and timezone."""
 		if not job['active']:
-			job['nextRunTime'] = 9999999999999 #todo, set to max or something
-			return
+			job['nextRunTime'] = 253402214400 #set to max value, only run just before the end of time
+			self.deleteJob(job['id'])	#just delete the job, until it's possible to edit schedules locally, inactive jobs has no place at all here
+			return False
 		today = date.today().weekday()
 		weekdays = [int(n) for n in job['weekdays'].split(',')]
 		runToday = False
@@ -67,7 +73,7 @@ class Scheduler(Plugin):
 			runTimeToday = self.calculateRunTimeForDay(todayDate, job)
 			if runTimeToday > time.time():
 				job['nextRunTime'] = runTimeToday + random.randint(0, job['random_interval']) * 60
-				return
+				return True
 			elif len(weekdays) == 1:
 				#this job should only run on this weekday, since it has already passed today, run it next week
 				runDate = todayDate + timedelta(days=7)
@@ -81,12 +87,12 @@ class Scheduler(Plugin):
 
 			if not runDate:
 				#something is wrong, no weekday to run
-				job['active'] = False
-				job['nextRunTime'] = 9999999999999 #todo, set to max or something
-				print "Error" #TODO
-				return
+				job['nextRunTime'] = 253402214400
+				self.deleteJob(job['id'])	#just delete the job, until it's possible to edit schedules locally, inactive jobs has no place at all here
+				return False
 
 		job['nextRunTime'] = self.calculateRunTimeForDay(runDate, job) + random.randint(0, job['random_interval']) * 60
+		return True
 		
 	def calculateNextWeekday(self, d, weekday):
 		days_ahead = weekday - d.weekday()
@@ -110,6 +116,42 @@ class Scheduler(Plugin):
 			riseSet = sunCalc.nextRiseSet(timegm(runDate.utctimetuple()), float(self.latitude), float(self.longitude))
 			return riseSet['sunset'] + job['offset'] * 60
 
+	def checkNewlyLoadedJob(self, job):
+		"""Checks if any of the jobs (local or initially loaded) should be running right now"""
+		if not job['active']:
+			return
+
+		weekdays = [int(n) for n in job['weekdays'].split(',')]
+		i = 0
+		while i < 2:
+			#Check today and yesterday (might be around 12 in the evening)
+			currentDate = date.today() + timedelta(days=-i)
+			if (currentDate.weekday() + 1) in weekdays:
+				#check for this day (today or yesterday)
+				runTime = self.calculateRunTimeForDay(currentDate, job)
+				runTimeMax = runTime + job['reps'] * 3 + job['retry_interval'] * 60 * (job['retries'] + 1) + 70 + job['random_interval'] * 60 + job['offset'] * 60
+				jobId = job['id']
+				executedJobs = self.s.get('executedJobs', {})
+				if (str(jobId) not in executedJobs or executedJobs[str(jobId)] < runTime) and  time.time() > runTime and time.time() < runTimeMax:
+					#run time for this job was passed during downtime, but it was passed within the max-runtime, and the last time it was executed (successfully) was before this run time, so it should be run again...
+					jobCopy = copy.deepcopy(job)
+					jobCopy['originalRepeats'] = job['reps']
+					jobCopy['nextRunTime'] = runTime
+					jobCopy['maxRunTime'] = runTimeMax #approximate maxRunTime, sanity check
+					self.runningJobs[jobId] = jobCopy
+					return
+			i = i + 1
+
+	def deleteJob(self, jobId):
+		with self.jobsLock:
+			self.jobs[:] = [x for x in self.jobs if x['id'] != jobId] #Test this! It should be fast and keep original reference, they say (though it will iterate all, even if it could end after one)
+			if jobId in self.runningJobs:	#TODO this might require a lock too?
+				self.runningJobs[jobId]['retries'] = 0
+
+			executedJobs = self.s.get('executedJobs', {})
+			if str(jobId) in executedJobs:
+				del executedJobs[str(jobId)]
+				self.s['executedJobs'] = executedJobs
 
 	def fetchLocalJobs(self):
 		"""Fetch local jobs from settings"""
@@ -139,47 +181,64 @@ class Scheduler(Plugin):
 		self.s['tz'] = self.timezone
 		self.calculateJobs(jobs)
 
+	@TelldusLive.handler('scheduler-update')
+	def receiveOneJobFromServer(self, msg):
+		"""Receive one job from server, add or edit, save to settings and calculate nextRunTime"""
+		if len(msg.argument(0).toNative()) == 0:
+			jobs = []
+		else:
+			scheduleDict = msg.argument(0).toNative()
+			job = scheduleDict['job']
+			self.timezone = scheduleDict['tz']
+
+		self.s['tz'] = self.timezone
+		active = self.calculateNextRunTime(job)
+		self.deleteJob(job['id']) #delete the job if it already exists (update)
+		if active:
+			with self.jobsLock:
+				self.jobs.append(job)
+				self.jobs.sort(key=lambda job: job['nextRunTime'])
+		self.s['jobs'] = self.jobs #save to storage
+		#self.live.pushToWeb('scheduler', 'updated', job['id']) #TODO is this a good idea? Trying to avoid cache problems where updates haven't come through? But this may not work if the same schedule is saved many times in a row, or if changes wasn't saved correctly to the database (not possible yet, only one database for schedules)
+
 	def requestJobsFromServer(self):
 		self.live.send(LiveMessage("scheduler-requestjob"))
 
 	def run(self):
 		self.running = True
+		time.sleep(10) #TODO have to have devices initiated before starting this (since we load schedules that should have been run during off)
 		while self.running:
-			#if len(self.jobs) > 0:
-			#	print str(self.jobs[0]['nextRunTime']), " vs ", str(time.time())
-			if len(self.jobs) > 0 and self.jobs[0]['nextRunTime'] < time.time():
-				#a job has passed its nextRunTime
-				job = self.jobs[0]
-				jobId = job['id']
-				#with self.runningJobsLock:
-				jobCopy = copy.deepcopy(job) #make a copy, don't edit the original job
+			jobCopy = None
+			with self.jobsLock:
+				if len(self.jobs) > 0 and self.jobs[0]['nextRunTime'] < time.time():
+					#a job has passed its nextRunTime
+					job = self.jobs[0]
+					jobId = job['id']
+					jobCopy = copy.deepcopy(job) #make a copy, don't edit the original job
+
+			if jobCopy:
 				jobCopy['originalRepeats'] = job['reps']
 				jobCopy['maxRunTime'] = jobCopy['nextRunTime'] + jobCopy['reps'] * 3 + jobCopy['retry_interval'] * 60 * (jobCopy['retries'] + 1) + 70 + jobCopy['random_interval'] * 60 + jobCopy['offset'] * 60 #approximate maxRunTime, sanity check
 				self.runningJobs[jobId] = jobCopy
 				self.calculateNextRunTime(job)
-				self.jobs.sort(key=lambda job: job['nextRunTime'])
-
-				#Retries: 	- 433: Abort if it has been sent
-							#  - ZWave: Abort if receiver sends ack
-				#Repeats: 433 only
-				#TODO When starting up, should we check if schedules are within retry interval and run them again if so? Or
-				#aviod that, since it could lead to that a job is run several times?
+				with self.jobsLock:
+					self.jobs.sort(key=lambda job: job['nextRunTime'])
 
 			jobsToRun = [] #jobs to run in a separate list, to avoid deadlocks (necessary?)
-			#with self.runningJobsLock: #or release it between each?
 			for runningJobId in self.runningJobs.keys():
 				runningJob = self.runningJobs[runningJobId]
 				if runningJob['nextRunTime'] < time.time():
 					if runningJob['maxRunTime'] > time.time():
-						isZWaveDevice = False #TODO
+						isZWaveDevice = True if 'zwave' in runningJob['transport'] else False
 						if not isZWaveDevice:
+							#repeats for 433-devices only, TODO test
 							runningJob['reps'] = int(runningJob['reps']) - 1
-							if runningJob['reps'] > 0: #TODO? >=
+							if runningJob['reps'] > 0:
 								runningJob['nextRunTime'] = time.time() + 3
 								jobsToRun.append(runningJob)
 								continue
 
-						if runningJob['retries'] > 1: #0 or 1?
+						if runningJob['retries'] > 0:
 							runningJob['nextRunTime'] = time.time() + (runningJob['retry_interval'] * 60)
 							runningJob['retries'] = runningJob['retries'] - 1
 							runningJob['reps'] = runningJob['originalRepeats']
