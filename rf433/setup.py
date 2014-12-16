d41089ec01ab7b52a0cc90bf39b60c1925058fcc
Micke Prag <micke.prag@telldus.se>
2014-12-16 16:44:58 +0100

    Remove namspace package in rf433

--- rf433/setup.py
+++ rf433/setup.py
@@ -12,7 +12,6 @@ setup(
 		'telldus.startup': ['c = rf433:RF433 [cREQ]'],
 	},
 	extras_require = dict(cREQ = 'Base>=0.1\nTelldus>=0.1\nTelldusLive>=0.1'),
-	namespace_packages = ['rf433'],
 	package_data={'rf433' : [
 		'firmware/TellStickDuo.hex'
 	]}
--- rf433/src/rf433/__init__.py
+++ rf433/src/rf433/__init__.py
@@ -1,5 +1,3 @@
 # -*- coding: utf-8 -*-
 
-__import__('pkg_resources').declare_namespace(__name__)
-
 from RF433 import RF433
