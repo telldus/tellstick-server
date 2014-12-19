82a6850f7710bff81db36d33806beedfbfcb1931
Micke Prag <micke.prag@telldus.se>
2014-12-19 15:22:38 +0100

    Add entry point that loads plugins witout the need for setuptools

---
+++ run-no-dep.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+plugins = {
+	'telldus': ['DeviceManager'],
+	'zwave.workaround': ['WorkaroundPlugin'],
+}
+startup = {
+	'log': ['Logger'],
+	'scheduler.base': ['Scheduler'],
+	'tellduslive.base': ['TelldusLive'],
+	'zwave.telldus': ['TelldusZWave'],
+}
+
+def loadClasses(cls):
+	classes = []
+	for module in cls:
+		m = __import__(module, globals(), locals(), cls[module])
+		for c in cls[module]:
+			classes.append(getattr(m, c))
+	return classes
+
+if __name__ == "__main__":
+	from base import Application
+
+	p = loadClasses(plugins)
+	s = loadClasses(startup)
+
+	app = Application(run=False)
+	app.run(startup=s)
