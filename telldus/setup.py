56599ce20357c8b99e771403527017cc76f59c7c
Micke Prag <micke.prag@telldus.se>
2014-12-19 13:37:48 +0100

    Remove dependency on setuptools for plugin telldus

--- telldus/setup.py
+++ telldus/setup.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from setuptools import setup, find_packages
+try:
+	from setuptools import setup
+except ImportError:
+	from distutils.core import setup
 
 setup(
 	name='Telldus',
 	version='0.1',
-	packages=find_packages('src'),
+	packages=['telldus'],
 	package_dir = {'':'src'},
 	extras_require = {
 		'telldus': ['Base>=0.1'],
