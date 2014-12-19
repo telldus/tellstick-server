fcb8251c3c3c0e3195afe834f39d0c016bde698b
Micke Prag <micke.prag@telldus.se>
2014-12-19 15:11:53 +0100

    Remove dependency on setuptools for plugin scheduler

--- scheduler/setup.py
+++ scheduler/setup.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from setuptools import setup, find_packages
+try:
+	from setuptools import setup
+except ImportError:
+	from distutils.core import setup
 
 setup(
 	name='Scheduler',
 	version='0.1',
-	packages=find_packages('src'),
+	packages=['scheduler'],
 	package_dir = {'':'src'},
 	entry_points={ \
 		'telldus.startup': ['c = scheduler.base:Scheduler [cREQ]']
