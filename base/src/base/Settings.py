f7c833c760dbf7dd9b7f0896f76fc40c6451da69
Micke Prag <micke.prag@telldus.se>
2015-01-08 12:18:38 +0100

    Do not write the settings file more often than once every 5 minutes. This is to save the flash tearing and minimize the risk of corruption

--- base/src/base/Settings.py
+++ base/src/base/Settings.py
@@ -2,13 +2,17 @@
 
 import os
 import json
+import time
+from Application import Application
 from configobj import ConfigObj
+from threading import Timer
 from board import Board
 import ConfigParser
 
-
 class Settings(object):
 	_config = None
+	_lastWrite = None
+	_writeTimer = None
 
 	def __init__(self, section):
 		super(Settings,self).__init__()
@@ -20,6 +24,7 @@ class Settings(object):
 				os.makedirs(self.configPath)
 			self.configFilename = 'Telldus.conf'
 			Settings._config = ConfigObj(self.configPath + '/' + self.configFilename)
+			Application().registerShutdown(self.__shutdown)
 		if section not in Settings._config:
 			Settings._config[section] = {}
 
@@ -33,6 +38,25 @@ class Settings(object):
 			v = int(v)
 		return v
 
+	def __shutdown(self):
+		if Settings._writeTimer is not None:
+			Settings._writeTimer.cancel()
+			self.__writeTimeout()
+
+	def __writeTimeout(self):
+		Settings._writeTimer = None
+		Settings._lastWrite = time.time()
+		Settings._config.write()
+
+	def __writeToDisk(self):
+		if Settings._writeTimer is not None:
+			return
+		if Settings._lastWrite is None or (time.time() - Settings._lastWrite) > 300:
+			Settings._writeTimer = Timer(1.0, self.__writeTimeout)
+		else:
+			Settings._writeTimer = Timer(300.0, self.__writeTimeout)
+		Settings._writeTimer.start()
+
 	def __getitem__(self, name):
 		try:
 			value = Settings._config[self.section][name]
@@ -44,4 +68,4 @@ class Settings(object):
 		if type(value) is dict or type(value) is list:
 			value = json.dumps(value)
 		Settings._config[self.section][name] = value
-		Settings._config.write()
+		self.__writeToDisk()
