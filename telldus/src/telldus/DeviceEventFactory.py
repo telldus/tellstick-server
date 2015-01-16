9a93a2745a5e72351f4b2033d1578b77110db838
Micke Prag <micke.prag@telldus.se>
2015-01-16 15:15:09 +0100

    Add sensor trigger

--- telldus/src/telldus/DeviceEventFactory.py
+++ telldus/src/telldus/DeviceEventFactory.py
@@ -10,7 +10,8 @@ class DeviceEventFactory(Plugin):
 	implements(IDeviceChange)
 
 	def __init__(self):
-		self.triggers = []
+		self.deviceTriggers = []
+		self.sensorTriggers = []
 		self.deviceManager = DeviceManager(self.context)
 
 	def createAction(self, type, params, **kwargs):
@@ -28,12 +29,21 @@ class DeviceEventFactory(Plugin):
 	def createTrigger(self, type, **kwargs):
 		if type == 'device':
 			trigger = DeviceTrigger(**kwargs)
-			self.triggers.append(trigger)
+			self.deviceTriggers.append(trigger)
+			return trigger
+		if type == 'sensor':
+			trigger = SensorTrigger(**kwargs)
+			self.sensorTriggers.append(trigger)
 			return trigger
 		return None
 
+	def sensorValueUpdated(self, device, valueType, value, scale):
+		for trigger in self.sensorTriggers:
+			if trigger.sensorId == device.id():
+				trigger.triggerSensorUpdate(valueType, value, scale)
+
 	def stateChanged(self, device, method, statevalue):
-		for trigger in self.triggers:
+		for trigger in self.deviceTriggers:
 			if trigger.deviceId == device.id() and trigger.method == int(method):
 				trigger.triggered()
 
@@ -104,3 +114,74 @@ class DeviceTrigger(Trigger):
 			self.deviceId = int(value)
 		elif name == 'method':
 			self.method = int(value)
+
+class SensorTrigger(Trigger):
+	def __init__(self, *args, **kwargs):
+		super(SensorTrigger, self).__init__(*args, **kwargs)
+		self.isTriggered = False
+		self.requireReload = False
+		self.reloadValue = 1
+		self.firstValue = True
+		self.scale = None
+
+	def parseParam(self, name, value):
+		if name == 'clientSensorId':
+			self.sensorId = int(value)
+		elif name == 'value':
+			self.value = float(value)
+		elif name == 'edge':
+			self.edge = int(value)
+		elif name == 'reloadValue':
+			self.reloadValue = min(15.0, max(0.1, float(value)))
+		elif name == 'scale':
+			self.scale = int(value)
+		elif name == 'valueType':
+			if value == 'temperature' or value == 'temp':
+				self.valueType = Device.TEMPERATURE
+			elif value == 'humidity':
+				self.valueType = Device.HUMIDITY
+			elif value == 'wgust':
+				self.valueType = Device.WINDGUST
+			elif value == 'rrate':
+				self.valueType = Device.RAINRATE
+			elif value == 'wavg':
+				self.valueType = Device.WINDAVERAGE
+			elif value == 'uv':
+				self.valueType = Device.UV
+			elif value == 'watt':
+				self.valueType = Device.WATT
+			elif value == 'lum':
+				self.valueType = Device.LUMINANCE
+
+	def triggerSensorUpdate(self, ttype, value, scale):
+		try:
+			if ttype != self.valueType:
+				return
+			if scale != self.scale:
+				return
+			value = float(value)
+			if not self.isTriggered:
+				if self.__compare(value, self.value, self.edge):
+					self.isTriggered = True
+					self.requireReload = True
+					if not self.firstValue:
+						self.triggered()
+			else:
+				if self.requireReload:
+					self.requireReload = abs(value-self.value) < self.reloadValue
+				if not self.requireReload:
+					if self.edge == 0:
+						self.isTriggered = False
+					else:
+						self.isTriggered = self.__compare(value, self.value, self.edge)
+			self.firstValue = False
+		except Exception as e:
+			pass
+
+	def __compare(self, value, trigger, edge):
+		if edge == 1:
+			return value > trigger
+		if edge == 0:
+			return value == trigger
+		if edge == -1:
+			return value < trigger
