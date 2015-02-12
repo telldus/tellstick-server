11d3dbdb6d06bae5761ed796148f6b4e8ff8fe45
Micke Prag <micke.prag@telldus.se>
2015-02-12 16:03:05 +0100

    Make plugin led distutils friendly

--- led/setup.py
+++ led/setup.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from setuptools import setup, find_packages
+try:
+	from setuptools import setup
+except ImportError:
+	from distutils.core import setup
 
 setup(
 	name='Led',
 	version='0.1',
-	packages=find_packages('src'),
+	packages=['led'],
 	package_dir = {'':'src'},
 	entry_points={ \
 		'telldus.startup': ['c = led:Led [cREQ]']
