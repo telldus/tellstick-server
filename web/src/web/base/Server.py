077c88ef7ef5948d385a9e3e5316640226d407e4
Micke Prag <micke.prag@telldus.se>
2015-02-26 16:01:47 +0100

    Add support to serve static web files usch as css and images

--- web/src/web/base/Server.py
+++ web/src/web/base/Server.py
@@ -1,7 +1,7 @@
-import cherrypy, threading
+import cherrypy, mimetypes, threading
 from base import Application, IInterface, ObserverCollection, Plugin
 from genshi.template import TemplateLoader, loader
-from pkg_resources import resource_filename
+from pkg_resources import resource_filename, resource_exists, resource_stream
 
 class IWebRequestHandler(IInterface):
 	"""Interface defenition for handling web requests"""
@@ -17,6 +17,7 @@ class IWebRequestHandler(IInterface):
 class Server(Plugin):
 	def __init__(self):
 		super(Server,self).__init__()
+		mimetypes.init()
 		cherrypy.config.update({
 			'server.socket_host': '0.0.0.0'
 		})
@@ -45,7 +46,15 @@ class RequestHandler(object):
 			self.templates = TemplateLoader(dirs)
 		return self.templates.load(filename)
 
-	def handle(self, *paths, **params):
+	def handle(self, plugin, p, **params):
+		path = '/'.join(p)
+		# First check for the file in htdocs
+		if plugin != '' and resource_exists(plugin, 'htdocs/' + path):
+			mimetype, encoding = mimetypes.guess_type(path, strict=False)
+			if mimetype is not None:
+				cherrypy.response.headers['Content-Type'] = mimetype
+			return resource_stream(plugin, 'htdocs/' + path)
+
 		menu = []
 		templates = []
 		for o in self.observers:
@@ -53,14 +62,15 @@ class RequestHandler(object):
 			if type(arr) == list:
 				menu.extend(arr)
 		for o in self.observers:
-			if o.matchRequest(paths):
-				template, data = o.handleRequest(paths, params)
+			if o.matchRequest(path):
+				template, data = o.handleRequest(path, params)
 				break
 		tmpl = self.loadTemplate(template)
 		data['menu'] = menu
 		stream = tmpl.generate(title='TellStick ZNet', **data)
 		return stream.render('html', doctype='html')
 
-	def __call__(self, *args, **kwargs):
-		return self.handle(*args, **kwargs)
+	def __call__(self, plugin = '', *args, **kwargs):
+		path = [x for x in args]
+		return self.handle(plugin, path, **kwargs)
 RequestHandler.exposed = True
