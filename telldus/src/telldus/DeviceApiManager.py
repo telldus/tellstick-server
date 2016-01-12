07f22ff044d7176cac433c7e05ee405990803068
Micke Prag <micke.prag@telldus.se>
2016-01-12 17:34:46 +0100

    Return methods, state and stateValue in devices/list

--- telldus/src/telldus/DeviceApiManager.py
+++ telldus/src/telldus/DeviceApiManager.py
@@ -2,27 +2,29 @@
 
 from api import IApiCallHandler, apicall
 from base import Plugin, implements
+from Device import Device
 from DeviceManager import DeviceManager
 
 class DeviceApiManager(Plugin):
 	implements(IApiCallHandler)
 
 	@apicall('devices', 'list')
-	def devicesList(self):
+	def devicesList(self, supportedMethods=0, **kwargs):
 		"""
-		Returns a list of all devices
+		Returns a list of all devices.
 		"""
 		deviceManager = DeviceManager(self.context)
 		retval = []
 		for d in deviceManager.retrieveDevices():
 			if not d.isDevice():
 				continue
+			state, stateValue = d.state()
 			retval.append({
 				'id': d.id(),
 				'name': d.name(),
-				'state': 0,
-				'statevalue': '',
-				'methods': 0,
+				'state': Device.maskUnsupportedMethods(state, int(supportedMethods)),
+				'statevalue': stateValue,
+				'methods': Device.maskUnsupportedMethods(d.methods(), int(supportedMethods)),
 				'type':'device',
 			})
 		return {'device': retval}
