91eb322c5c176fde302131e76ce60fa85489f6a5
Micke Prag <micke.prag@telldus.se>
2016-01-18 14:24:56 +0100

    Do not require authentication for lua scripts (yet)

--- lua/src/lua/Lua.py
+++ lua/src/lua/Lua.py
@@ -102,6 +102,9 @@ class Lua(Plugin):
 		for s in self.scripts:
 			s.load()
 
+	def requireAuthentication(self, plugin, path):
+		return plugin != 'lua'
+
 	def saveScript(self, scriptName, code):
 		for script in self.scripts:
 			if script.name != scriptName:
