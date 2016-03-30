a38f9a1cc280a22b2ae421ae3bd427c9473800d0
Micke Prag <micke.prag@telldus.se>
2016-03-30 10:47:29 +0200

    Add function Board:__cfg() to make it easier to have different configuration for various boards

--- board/openwrt/board/Board.py
+++ board/openwrt/board/Board.py
@@ -4,6 +4,19 @@
 import os
 
 class Board(object):
+	cfgs = {
+		'tellstick-znet-lite': {
+			'secret': {'part': '/dev/mtd0', 'offset': 0x1FC20},
+			'433': '/dev/ttyUSB0',
+			'z-wave': '/dev/ttyACM0',
+		},
+		'tellstick-znet-fika': {
+			'secret': {'part': '/dev/mtd2', 'offset': 0x80},
+			'433': '/dev/ttyUSB0',
+			'z-wave': '/dev/ttyACM0',
+		},
+	}
+
 	@staticmethod
 	def configDir():
 		return '/etc'
@@ -41,21 +54,17 @@ class Board(object):
 
 	@staticmethod
 	def rf433Port():
-		return '/dev/ttyUSB0'
+		return Board.__cfg('433')
 
 	@staticmethod
 	def zwavePort():
-		return '/dev/ttyACM0'
+		return Board.__cfg('z-wave')
 
 	@staticmethod
 	def secret():
-		cfgs = {
-			'tellstick-znet-lite': {'part': '/dev/mtd0', 'offset': 0x1FC20},
-			'tellstick-znet-fika': {'part': '/dev/mtd2', 'offset': 0x80},
-		}
-		if Board.hw() not in cfgs:
+		cfg = Board.__cfg('secret')
+		if cfg is None:
 			return ''
-		cfg = cfgs[Board.hw()]
 		with open(cfg['part'], 'rb') as f:
 			f.seek(cfg['offset'])
 			return f.read(10)
@@ -79,3 +88,13 @@ class Board(object):
 	def doUpgradeImage(type, path):
 		if type == 'firmware':
 			os.system("/sbin/sysupgrade %s" % path)
+
+	@staticmethod
+	def __cfg(name, default = None):
+		hw = Board.hw()
+		if hw not in Board.cfgs:
+			return default
+		cfg = Board.cfgs[hw]
+		if name not in cfg:
+			return default
+		return cfg[name]
