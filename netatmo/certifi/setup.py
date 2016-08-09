04543f037cc01702e7bd07b8eff94dcfe4678fef
Micke Prag <micke.prag@telldus.se>
2016-08-09 13:49:21 +0200

    Add dummy certifi plugin to be used for the requests library to find the certificates in the image

---
+++ netatmo/certifi/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+try:
+	from setuptools import setup
+except ImportError:
+	from distutils.core import setup
+
+setup(
+	name='certifi',
+	version='0.1',
+	py_modules=['certifi'],
+)
