466953ad7b32c5098e1a9b02cc309b2b6716c7f7
Micke Prag <micke.prag@telldus.se>
2016-10-11 16:16:31 +0200

    Add websocket support for react applications

--- web/src/web/app/components/PluginLoader.jsx
+++ web/src/web/app/components/PluginLoader.jsx
@@ -53,7 +53,79 @@ requirejs.define('telldus', function () {
 	};
 });
 requirejs.define('dialog-polyfill', dialogpolyfill);
+requirejs.define('websocket', function() {
+	var onMessage = [];
 
+	var WebSocketWrapper = function() {
+		console.log("Create new websocket main object");
+		this.setup();
+	}
+
+	WebSocketWrapper.prototype.setup = function() {
+		this.ws = new WebSocket('ws://' + location.host + '/ws');
+		this.ws.onopen = function() {
+			console.log("Websocket opened");
+		};
+
+		this.ws.onmessage = function (evt) {
+			var obj = JSON.parse( evt.data );
+			for(var i = 0; i < onMessage.length; ++i) {
+				onMessage[i].messageReceived(obj['module'], obj['action'], obj['data']);
+			}
+		};
+
+		this.ws.onclose = function() {
+			// websocket is closed.
+			console.log("Websocket closed");
+		};
+	}
+
+	WebSocketWrapper.prototype.register = function (evt) {
+		onMessage.push(evt);
+	};
+
+	WebSocketWrapper.prototype.unregister = function (evt) {
+		var index = onMessage.indexOf(evt)
+		if (index >= 0) {
+			onMessage.splice(index, 1);
+		}
+	};
+
+	var ws = new WebSocketWrapper();
+
+	var WebSocketInstance = function() {
+		this.filter = [];
+	}
+
+	WebSocketInstance.prototype.messageReceived = function(module, action, data) {
+		for(var i = 0; i < this.filter.length; ++i) {
+			if (this.filter[i].module === module && this.filter[i].action === action) {
+				this.filter[i].fn(module, action, data);
+			}
+		}
+	}
+
+	WebSocketInstance.prototype.onMessage = function (module, action, evt) {
+		if (evt == null) {
+			for(var i = 0; i < this.filter.length; ++i) {
+				if (this.filter[i].module === module && this.filter[i].action === action) {
+					this.filter.splice(i, 1);
+					break;
+				}
+			}
+			if (this.filter.length == 0) {
+				ws.unregister(this);
+			}
+		} else {
+			if (this.filter.length == 0) {
+				ws.register(this);
+			}
+			this.filter.push({module: module, action: action, fn: evt});
+		}
+	};
+
+	return WebSocketInstance;
+})
 
 class Placeholder extends React.Component {
 	render() {
