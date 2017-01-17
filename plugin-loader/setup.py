5a57156a5ac1334b9ae96203131664de8945527b
Micke Prag <micke.prag@telldus.se>
2017-01-17 12:20:45 +0100

    Throw an error if we cannot build the web application

--- plugin-loader/setup.py
+++ plugin-loader/setup.py
@@ -12,8 +12,10 @@ import os
 class buildweb(install):
 	def run(self):
 		print("generate web application")
-		os.system('npm install')
-		os.system('npm run build')
+		if os.system('npm install') != 0:
+			raise Exception("Could not install npm packages")
+		if os.system('npm run build') != 0:
+			raise Exception("Could not build web application")
 		install.run(self)
 
 setup(
