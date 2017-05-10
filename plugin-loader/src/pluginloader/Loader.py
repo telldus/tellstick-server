1256af6bfcd2d28842206c3ab7d8b553a1038ca5
Micke Prag <micke.prag@telldus.se>
2017-05-10 14:51:24 +0200

    Fix styles for pylint

--- plugin-loader/src/pluginloader/Loader.py
+++ plugin-loader/src/pluginloader/Loader.py
@@ -3,23 +3,26 @@
 from base import Application, Plugin, mainthread, ConfigurationManager
 from board import Board
 from web.base import Server
-from PluginParser import PluginParser
 import glob
-import gnupg
 import logging
 import os
-import pkg_resources
 import shutil
 import sys
 import threading
 import time
 import traceback
-import urllib, urllib2
+import urllib
+import urllib2
 import yaml
 import zipfile
+import pkg_resources
+from .PluginParser import PluginParser
+import gnupg
+
 
 def loadGPG():
 	return gnupg.GPG(keyring='%s/plugins.keyring' % Board.pluginPath())
+
 
 class LoadedPlugin(object):
 	def __init__(self, manifest, context):
@@ -50,16 +53,18 @@ class LoadedPlugin(object):
 			'config': configs,
 			'long_description': self.manifest.get('long_description', ''),
 			'description': self.manifest.get('description', ''),
-			'icon': '/pluginloader/icon?%s' % urllib.urlencode({'name': self.name}) if self.icon != '' else '',
+			'icon': '/pluginloader/icon?%s' %
+				urllib.urlencode({'name': self.name}) if self.icon != '' else '',
 			'loaded': self.loaded,
 			'name': self.name,
 			'size': self.size(),
 			'version': self.manifest.get('version', ''),
 		}
 
-	def printBacktrace(self, bt):
-		for f in bt:
-			logging.error(str(f))
+	@staticmethod
+	def printBacktrace(backtrace):
+		for func in backtrace:
+			logging.error(str(func))
 
 	def remove(self):
 		shutil.rmtree(self.path)
@@ -75,13 +80,13 @@ class LoadedPlugin(object):
 
 	def size(self):
 		size = 0
-		for f in os.listdir(self.path):
-			size += os.path.getsize(os.path.join(self.path, f))
+		for filename in os.listdir(self.path):
+			size += os.path.getsize(os.path.join(self.path, filename))
 		return size
 
 	def verify(self):
-		for p in self.packages:
-			if not LoadedPlugin.__verifyFile('%s/%s' % (self.path, p)):
+		for package in self.packages:
+			if not LoadedPlugin.__verifyFile('%s/%s' % (self.path, package)):
 				return False
 		return True
 
@@ -90,11 +95,12 @@ class LoadedPlugin(object):
 		try:
 			if not self.verify():
 				return
-		except Exception as e:
-			logging.warning("Could not load plugin %s: %s", self.name, str(e))
+		# pylint: disable=broad-except
+		except Exception as exception:
+			logging.warning("Could not load plugin %s: %s", self.name, str(exception))
 			return
-		for p in self.packages:
-			self.__loadEgg(p)
+		for package in self.packages:
+			self.__loadEgg(package)
 		# TODO: Do not just set the loaded flag here. Make sure the eggs where loaded and store any
 		# backtrace if the loading failed.
 		self.loaded = True
@@ -110,10 +116,11 @@ class LoadedPlugin(object):
 				try:
 					moduleClass = info.load()
 					self.classes.append(moduleClass)
-				except Exception as e:
-					exc_type, exc_value, exc_traceback = sys.exc_info()
+				# pylint: disable=broad-except
+				except Exception as exception:
+					_exc_type, _exc_value, exc_traceback = sys.exc_info()
 					logging.error("Could not load %s", str(entry))
-					logging.error(str(e))
+					logging.error(str(exception))
 					self.printBacktrace(traceback.extract_tb(exc_traceback))
 
 			for entry in dist.get_entry_map(group='telldus.startup'):
@@ -122,22 +129,23 @@ class LoadedPlugin(object):
 					moduleClass = info.load()
 					self.classes.append(moduleClass)
 					if issubclass(moduleClass, Plugin):
-						m = moduleClass(self.context)
+						moduleClass(self.context)
 					else:
-						m = moduleClass()
-				except Exception as e:
-					exc_type, exc_value, exc_traceback = sys.exc_info()
+						moduleClass()
+				# pylint: disable=broad-except
+				except Exception as exception:
+					_exc_type, _exc_value, exc_traceback = sys.exc_info()
 					logging.error("Could not load %s", str(entry))
-					logging.error(str(e))
+					logging.error(str(exception))
 					self.printBacktrace(traceback.extract_tb(exc_traceback))
 
 	@staticmethod
 	def __verifyFile(path):
 		# Check signature
 		gpg = loadGPG()
-		v = gpg.verify_file(open('%s.asc' % path, 'rb'), path)
-		if v.valid is not True:
-			raise Exception('Could not verify signature: %s' % v.status)
+		result = gpg.verify_file(open('%s.asc' % path, 'rb'), path)
+		if result.valid is not True:
+			raise Exception('Could not verify signature: %s' % result.status)
 		return True
 
 class Loader(Plugin):
@@ -146,7 +154,7 @@ class Loader(Plugin):
 		self.initializeKeychain()
 		self.loadPlugins()
 		Application().registerScheduledTask(self.updatePluginsList, days=7)
-		if os.path.exists('%s/plugins.yml' % Board.pluginPath()) == False:
+		if not os.path.exists('%s/plugins.yml' % Board.pluginPath()):
 			# Run now
 			self.updatePluginsList()
 		elif time.time() - os.path.getmtime('%s/plugins.yml' % Board.pluginPath()) >= 604800:
@@ -160,10 +168,10 @@ class Loader(Plugin):
 			return {'success': False, 'msg': 'No plugin uploaded'}
 		try:
 			gpg = loadGPG()
-			with zipfile.ZipFile(filename, 'r') as z:
-				cfg = yaml.load(z.read('manifest.yml'))
-				k = z.extract(cfg['key'], '/tmp/')
-				keys = gpg.scan_keys(k)
+			with zipfile.ZipFile(filename, 'r') as zipF:
+				cfg = yaml.load(zipF.read('manifest.yml'))
+				keyFile = zipF.extract(cfg['key'], '/tmp/')
+				keys = gpg.scan_keys(keyFile)
 				if len(keys) != 1:
 					raise Exception('Key must only contain exactly one public key')
 				key = keys[0]
@@ -172,24 +180,26 @@ class Loader(Plugin):
 				keyid = key['keyid']
 				if keyid != acceptKeyId:
 					return {'name': name, 'fingerprint': fingerprint, 'keyid': keyid}
-				result = gpg.import_keys(open(k).read())
-				os.unlink(k)
+				gpg.import_keys(open(keyFile).read())
+				os.unlink(keyFile)
 				# Reload loaded keys
+				# pylint: disable=too-many-function-args
 				Loader(self.context).initializeKeychain()
-		except Exception as e:
+		# pylint: disable=broad-except
+		except Exception as exception:
 			os.unlink(filename)
-			return {'success': False, 'msg': str(e)}
+			return {'success': False, 'msg': str(exception)}
 		return {'success': True}
 
 	def importPlugin(self, filename):
-		z = None
+		zipF = None
 		try:
-			z = zipfile.ZipFile(filename, 'r')
+			zipF = zipfile.ZipFile(filename, 'r')
 			try:
-				info = z.getinfo('manifest.yml')
+				info = zipF.getinfo('manifest.yml')
 			except KeyError:
 				raise ImportError('Malformed plugin. No manifest found.')
-			cfg = yaml.load(z.read('manifest.yml'))
+			cfg = yaml.load(zipF.read('manifest.yml'))
 			if 'name' not in cfg:
 				raise ImportError('Malformed plugin. Plugin has no name.')
 			if cfg['name'] == 'staging':
@@ -198,43 +208,48 @@ class Loader(Plugin):
 				raise ImportError('Malformed plugin. Manifest does not list any packages.')
 			try:
 				gpg = loadGPG()
-			except Exception as e:
-				raise ImportError(str(e))
+			except Exception as exception:
+				raise ImportError(str(exception))
 			packages = []
-			for p in cfg['packages']:
-				f = z.extract(p, '/tmp/')
-				s = z.getinfo('%s.asc' % p)
-				packages.append((f, s,))
-				result = gpg.verify_file(z.open(s), f)
+			for package in cfg['packages']:
+				packageFilename = zipF.extract(package, '/tmp/')
+				signature = zipF.getinfo('%s.asc' % package)
+				packages.append((packageFilename, signature,))
+				result = gpg.verify_file(zipF.open(signature), packageFilename)
 				if result.valid is True:
 					continue
 				# remove unpackaged files
-				for p, s in packages:
-					os.unlink(p)
+				for packageFilename, _signature in packages:
+					os.unlink(packageFilename)
 				if result.pubkey_fingerprint is None and result.username is None:
 					# No public key for this plugin
-					#return {'success': False, 'key': self.importKey(None)}
-					pass #  Do not allow importing of custom keys yet.
-				raise ImportError('Could not verify plugin. Please make sure this plugin was downloaded from a trusted source.')
+					return {'success': False, 'key': self.importKey(None)}
+				raise ImportError(
+					'Could not verify plugin. Please make sure this plugin was downloaded from a trusted source.'
+				)
 			path = '%s/%s' % (Board.pluginPath(), cfg['name'])
 			if os.path.exists(path):
 				# Wipe any old plugin
 				shutil.rmtree(path)
 			os.mkdir(path)
-			for p, s in packages:
-				shutil.move(p, '%s/%s' % (path, os.path.basename(p)))
-				z.extract(s, path)
-			manifest = z.extract(info, path)
+			for packageFilename, signature in packages:
+				shutil.move(packageFilename, '%s/%s' % (path, os.path.basename(packageFilename)))
+				zipF.extract(signature, path)
+			manifest = zipF.extract(info, path)
 			if 'icon' in cfg:
-				z.extract(cfg['icon'], path)
+				zipF.extract(cfg['icon'], path)
 		except zipfile.BadZipfile:
 			raise ImportError('Uploaded file was not a Zip file')
 		finally:
-			if z is not None:
-				z.close()
+			if zipF is not None:
+				zipF.close()
 		os.unlink(filename)
 		status = self.loadPlugin(manifest)
-		return {'success': True, 'msg': 'Plugin was imported', 'restartRequired': True if status == 1 else False}
+		return {
+			'success': True,
+			'msg': 'Plugin was imported',
+			'restartRequired': True if status == 1 else False
+		}
 
 	def initializeKeychain(self):
 		filename = pkg_resources.resource_filename('pluginloader', 'files/telldus.gpg')
@@ -254,56 +269,65 @@ class Loader(Plugin):
 		filename = '%s/staging.zip' % Board.pluginPath()
 		server = Server(self.context)
 		def downloadFile():
-			u = urllib2.urlopen(url)
-			meta = u.info()
+			urlFd = urllib2.urlopen(url)
+			meta = urlFd.info()
 			fileSize = int(meta.getheaders("Content-Length")[0])
 			if size is not None and fileSize != size:
 				raise Exception("Size mismatch")
-				return False
-			f = open(filename, 'wb')
+			fd = open(filename, 'wb')
 			fileSizeDl = 0
 			blockSz = 8192
 			server.webSocketSend('plugins', 'downloadProgress', {'downloaded': fileSizeDl, 'size': fileSize})
 			while True:
-				buffer = u.read(blockSz)
-				if not buffer:
-						break
-				fileSizeDl += len(buffer)
-				f.write(buffer)
-				server.webSocketSend('plugins', 'downloadProgress', {'downloaded': fileSizeDl, 'size': fileSize})
-			f.close()
+				buff = urlFd.read(blockSz)
+				if not buff:
+					break
+				fileSizeDl += len(buff)
+				fd.write(buff)
+				server.webSocketSend(
+					'plugins',
+					'downloadProgress',
+					{'downloaded': fileSizeDl, 'size': fileSize}
+				)
+			fd.close()
 			return True
 		def install():
 			logging.warning('Install plugin %s from %s', name, url)
 			try:
 				downloadFile()
-			except Exception as e:
-				server.webSocketSend('plugins', 'downloadFailed', {'msg': str(e)})
+			# pylint: disable=broad-except
+			except Exception as exception:
+				server.webSocketSend('plugins', 'downloadFailed', {'msg': str(exception)})
 				return
 			try:
 				msg = self.importPlugin(filename)
 				server.webSocketSend('plugins', 'install', msg)
-			except ImportError as e:
+			except ImportError as error:
 				os.unlink(filename)
-				server.webSocketSend('plugins', 'install', {'success': False, 'msg':'Error importing plugin: %s' % e})
-		t = threading.Thread(name='Plugin installer', target=install)
-		t.daemon = True  # Kill if needed
-		t.start()
+				server.webSocketSend(
+					'plugins',
+					'install',
+					{'success': False, 'msg':'Error importing plugin: %s' % error}
+				)
+		thread = threading.Thread(name='Plugin installer', target=install)
+		thread.daemon = True  # Kill if needed
+		thread.start()
 
 	def loadPlugin(self, manifest):
 		plugin = LoadedPlugin(manifest, self.context)
 		plugin.verifyAndLoad()
-		for i, p in enumerate(self.plugins):
-			if p.name == plugin.name:
+		for i, loadedPlugin in enumerate(self.plugins):
+			if loadedPlugin.name == plugin.name:
 				self.plugins[i] = plugin
 				return 1
 		self.plugins.append(plugin)
 		return 0
 
 	def loadPlugins(self):
-		for f in glob.glob('%s/**/manifest.yml' % Board.pluginPath()):
-			self.loadPlugin(f)
+		for filename in glob.glob('%s/**/manifest.yml' % Board.pluginPath()):
+			self.loadPlugin(filename)
 
+	# pylint: disable=unused-argument
 	def removeKey(self, key, fingerprint):
 		gpg = loadGPG()
 		gpg.delete_keys(fingerprint, True)
@@ -329,14 +353,14 @@ class Loader(Plugin):
 
 	def updatePluginsList(self):
 		# Run in a separate thread to allow blocking calls
-		t = threading.Thread(target=self.__updatePluginsList, name='PluginList updater')
-		t.daemon = True
-		t.start()
+		thread = threading.Thread(target=self.__updatePluginsList, name='PluginList updater')
+		thread.daemon = True
+		thread.start()
 
 	def __updatePluginsList(self):
 		parser = PluginParser()
 		data = parser.parse()
-		with open('%s/plugins.yml' % Board.pluginPath(), 'w') as f:
-			yaml.dump(data, f, default_flow_style=False)
+		with open('%s/plugins.yml' % Board.pluginPath(), 'w') as fd:
+			yaml.dump(data, fd, default_flow_style=False)
 		# Notify clients through websocket
 		Server(self.context).webSocketSend('plugins', 'storePluginsUpdated', None)
