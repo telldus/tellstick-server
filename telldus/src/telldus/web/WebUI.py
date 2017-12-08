24ac94153c8da67a1b2468aeeca7f43aae7b21d7
Micke Prag <micke.prag@telldus.se>
2017-12-08 13:54:23 +0100

    Add functions to change the distribution
    Changelog: Add option to enable beta firmware

--- telldus/src/telldus/web/WebUI.py
+++ telldus/src/telldus/web/WebUI.py
@@ -1,3 +1,5 @@
+import os
+
 from base import Plugin, implements
 from board import Board
 from web.base import IWebRequestHandler, WebResponseJson
@@ -9,7 +11,8 @@ class WebUI(Plugin):
 	def __init__(self):
 		pass
 
-	def getReactComponents(self):
+	@staticmethod
+	def getReactComponents():
 		retval = {
 			'com.telldus.firmware': {
 				'title': 'Firmware',
@@ -19,7 +22,8 @@ class WebUI(Plugin):
 		}
 		return retval
 
-	def handleRequest(self, plugin, path, params, request):
+	@staticmethod
+	def handleRequest(plugin, path, params, **__kwargs):
 		if plugin != 'telldus':
 			return None
 
@@ -28,7 +32,7 @@ class WebUI(Plugin):
 				with open('/etc/distribution') as fd:
 					distribution = fd.readline().strip()
 			except Exception as __error:
-				distribution = 'unknown'
+				distribution = ''
 			return WebResponseJson({
 				'firmware': {
 					'version': Board.firmwareVersion(),
@@ -36,11 +40,21 @@ class WebUI(Plugin):
 				},
 			})
 
+		if path == 'setDistribution':
+			if params.get('name', '') not in ['beta', 'stable']:
+				return WebResponseJson({'success': False, 'error': 'Invalid distribution'})
+			retval = os.system('/usr/sbin/telldus-helper distribution %s' % params['name'])
+			if retval == 0:
+				return WebResponseJson({'success': True})
+			return WebResponseJson({'success': False, 'error': 'Could not change the firmware version'})
+
 		return None
 
-	def matchRequest(self, plugin, path):
+	@staticmethod
+	def matchRequest(plugin, path):
 		if plugin == 'telldus' and path in [
 			'info',
+			'setDistribution'
 		]:
 			return True
 		return False
--- telldus/src/telldus/web/app/actions/settings.jsx
+++ telldus/src/telldus/web/app/actions/settings.jsx
@@ -1,5 +1,7 @@
 export const REQUEST_TELLDUS_INFO = 'REQUEST_TELLDUS_INFO'
 export const RECEIVE_TELLDUS_INFO = 'RECEIVE_TELLDUS_INFO'
+export const REQUEST_SET_DISTRIBUTION = 'REQUEST_SET_DISTRIBUTION'
+export const RECEIVE_SET_DISTRIBUTION = 'RECEIVE_SET_DISTRIBUTION'
 
 function requestTelldusInfo() { return { type: REQUEST_TELLDUS_INFO} }
 function receiveTelldusInfo(json) { return { type: RECEIVE_TELLDUS_INFO, info: json } }
@@ -11,5 +13,20 @@ export function fetchTelldusInfo() {
 		})
 			.then(response => response.json())
 			.then(json => dispatch(receiveTelldusInfo(json)))
+	}
+}
+
+export function setDistribution(name) {
+	var data = new FormData();
+	data.append('name', name);
+	return dispatch => {
+		dispatch({ type: REQUEST_SET_DISTRIBUTION} )
+		return fetch('/telldus/setDistribution', {
+			method: 'POST',
+			credentials: 'include',
+			body: data,
+		})
+			.then(response => response.json())
+			.then(json => dispatch({ type: RECEIVE_SET_DISTRIBUTION, info: json }))
 	}
 }
