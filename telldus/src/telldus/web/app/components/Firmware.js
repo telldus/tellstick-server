e08140bb58bbeed7fdf349e0bad199773b9aeec3
Micke Prag <micke.prag@telldus.se>
2017-12-08 13:54:23 +0100

    Add functions to change the distribution
    Changelog: Add option to enable beta firmware

--- telldus/src/telldus/web/app/components/Firmware.js
+++ telldus/src/telldus/web/app/components/Firmware.js
@@ -1,10 +1,72 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Button, CardActions, CardText } from 'react-mdl';
+import { Button, CardActions, CardText, Dialog, DialogActions, DialogContent, DialogTitle, Spinner } from 'react-mdl';
+
+import { setDistribution } from '../actions/settings'
+
+class RebootDialog extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+	componentDidMount() {
+		if (!this.dialog.dialogRef.showModal) {
+			DialogPolyfill.registerDialog(this.dialog.dialogRef);
+		}
+	}
+	render() {
+		return (
+			<Dialog style={{
+				width: '500px',
+				padding: '0'
+			}} open={this.props.open} ref={c => {this.dialog = c}}>
+				<DialogTitle style={{
+					color: '#555'
+				}}>
+					<span>Confirm change to {this.props.distribution}</span>
+				</DialogTitle>
+				<DialogContent>
+					<p>Your TellStick needs to be rebooted before the changes will take effect.</p>
+					<p>Rebooting will take about 10 minutes or more.</p>
+					<p>Do not disconnect the power to your TellStick while upgrading. This may break your TellStick</p>
+					<p>Your TellStick will not be accessible during the upgrade</p>
+					<div style={{color: 'red', display: this.props.rebootStatus == -1 ? '' : 'none'}}>Error. Could not reboot device.</div>
+				</DialogContent>
+				<DialogActions style={{
+					backgroundColor: '#757575',
+					padding: '12px'
+				}}>
+					<Button className="buttonRounded buttonWhite" onClick={() => this.props.onClose()} raised>Abort</Button>
+					<Button raised className="buttonRounded buttonAccept" disabled={this.props.rebootStatus > 0} onClick={() => this.props.onReboot()} style={{
+						backgroundColor: '#9ccc65'
+					}}>Reboot now</Button>
+					<Spinner style={{display: (this.props.rebootStatus == 1 ? '' : 'none')}}/>
+				</DialogActions>
+			</Dialog>
+		)
+	}
+}
 
 class Firmware extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			'rebootShown': false,
+			'rebootStatus': 0,
+			'distribution': '',
+		}
+	}
 	updateDistribution(name) {
-		// TODO
+		this.setState({
+			'rebootShown': true,
+			'rebootStatus': 0,
+			'distribution': name,
+		});
+	}
+	setAndReboot() {
+		this.setState({
+			'rebootStatus': 1
+		});
+		this.props.onSetDistribution(this.state.distribution);
 	}
 	render() {
 		return (
@@ -16,6 +78,13 @@ class Firmware extends React.Component {
 					{this.props.distribution != 'beta' && <Button onClick={() => this.updateDistribution('beta')} colored>Switch to beta</Button>}
 					{this.props.distribution != 'stable' && <Button onClick={() => this.updateDistribution('stable')} colored>Switch to stable</Button>}
 				</CardActions>
+				<RebootDialog
+					open={this.state.rebootShown}
+					distribution={this.state.distribution}
+					rebootStatus={this.state.rebootStatus}
+					onClose={() => this.setState({'rebootShown': false})}
+					onReboot={() => this.setAndReboot()}
+				/>
 			</div>
 		)
 	}
@@ -27,5 +96,6 @@ const mapStateToProps = (state) => {
   }
 }
 const mapDispatchToProps = (dispatch) => ({
+	onSetDistribution: (name) => dispatch(setDistribution(name)),
 });
 export const FirmwareSettings = connect(mapStateToProps, mapDispatchToProps)(Firmware)
