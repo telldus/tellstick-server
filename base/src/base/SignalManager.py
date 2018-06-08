23162332ed9a37adb6c662ca5446867a2255db30
Micke Prag <micke.prag@telldus.se>
2018-06-08 11:58:33 +0200

    Pylint fixes

--- base/src/base/SignalManager.py
+++ base/src/base/SignalManager.py
@@ -1,60 +1,62 @@
 # -*- coding: utf-8 -*-
 
+import sys
+import types
+
 from .Application import Application
 from .Plugin import IInterface, ObserverCollection, Plugin
-import sys, types
 
 class ISignalObserver(IInterface):
 	"""Implement this IInterface to recieve signals using the decorator :py:func:`@slot <base.slot>`"""
 
 class Signal(object):
 	def __init__(self, fn):
-		self.fn = fn
+		self.func = fn
 
 	def doc(self):
-		doc = self.fn.__doc__ if self.fn.__doc__ is not None else ''
+		doc = self.func.__doc__ if self.func.__doc__ is not None else ''
 		return doc.strip()
 
 	def args(self):
-		return list(self.fn.func_code.co_varnames)[1:self.fn.func_code.co_argcount]
+		return list(self.func.func_code.co_varnames)[1:self.func.func_code.co_argcount]
 
 class SignalManager(Plugin):
 	observers = ObserverCollection(ISignalObserver)
 	signals = {}
 
 	def sendSignal(self, msg, *args, **kwargs):
-		for o in self.observers:
-			for f in getattr(o, '_applicationSlots', {}).get(msg, []):
-				Application().queue(f, o, *args, **kwargs)
-			for f in getattr(o, '_applicationSlots', {}).get('', []):
-				Application().queue(f, o, msg, *args, **kwargs)
+		for observer in self.observers:
+			for func in getattr(observer, '_applicationSlots', {}).get(msg, []):
+				Application().queue(func, observer, *args, **kwargs)
+			for func in getattr(observer, '_applicationSlots', {}).get('', []):
+				Application().queue(func, observer, msg, *args, **kwargs)
 
 	@staticmethod
-	def slot(message = ''):
-		def call(fn):
-			frame = sys._getframe(1)
-			frame.f_locals.setdefault('_applicationSlots', {}).setdefault(message, []).append(fn)
-			return fn
+	def slot(message=''):
+		def call(func):
+			frame = sys._getframe(1)  # pylint: disable=W0212
+			frame.f_locals.setdefault('_applicationSlots', {}).setdefault(message, []).append(func)
+			return func
 		return call
 
 	@staticmethod
-	def signal(name = None):
-		def call(fn, *args, **kwargs):
-			SignalManager.signals[name if type(name) is str else fn.__name__] = Signal(fn)
+	def signal(name=None):
+		def call(func, *__args, **__kwargs):
+			SignalManager.signals[name if isinstance(name, str) else func.__name__] = Signal(func)
 			def wrapper(obj, *args, **kwargs):
-				signalName = name if type(name) is str else fn.__name__
+				signalName = name if isinstance(name, str) else func.__name__
 				ctx = obj.context if hasattr(obj, 'context') else Application().pluginContext
-				retval = fn(obj, *args, **kwargs)
+				retval = func(obj, *args, **kwargs)
 				SignalManager(ctx).sendSignal(signalName, *args, **kwargs)
 				return retval
 			return wrapper
-		if type(name) is str or name is None:
+		if isinstance(name, str) or name is None:
 			# decorator is used like this @signal() or @signal('name')
 			return call
-		if type(name) is types.FunctionType:
+		if isinstance(name, types.FunctionType):
 			# decorator is used like this @signal
 			return call(name)
 		return None
 
-signal = SignalManager.signal
-slot = SignalManager.slot
+signal = SignalManager.signal  # pylint: disable=C0103
+slot = SignalManager.slot  # pylint: disable=C0103
