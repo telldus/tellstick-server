31c59cfc0284f560db0e57bf40dd03e35dedc2c8
odavinash <odavinash786@gmail.com>
2018-09-04 14:33:03 +0530

    Configuration drop down menu
    This Configuration will support for the drop-down menu. And user able to select the value from drop-down for plugin value like select country, timezone etc.

---
+++ plugin-loader/src/pluginloader/ConfigurationDropDown.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+from telldus.web import ConfigurationReactComponent
+import copy
+import json
+import rauth
+
+class ConfigurationDropDown(ConfigurationReactComponent):
+	def __init__(self,
+		menuList,
+		selected=None,
+		**kwargs
+	):
+		super(ConfigurationDropDown, self).__init__('plugins/dropdown', defaultValue={"selected":selected,"list":menuList}, **kwargs)
+		self.menuList = menuList or {}
+		self.selected = selected
--- plugin-loader/src/pluginloader/__init__.py
+++ plugin-loader/src/pluginloader/__init__.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
 from .ConfigurationOAuth2 import ConfigurationOAuth2
+from .ConfigurationDropDown import ConfigurationDropDown
 from .Loader import Loader
 from .WebFrontend import WebFrontend
