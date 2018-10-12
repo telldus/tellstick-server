7be6d9490c1ae5375a8fa3f837edcaf46a68248b
Micke Prag <micke.prag@telldus.se>
2018-10-12 15:18:51 +0200

    Add unit-testing to CI

--- tellstick.sh
+++ tellstick.sh
@@ -99,6 +99,7 @@ printHelp() {
 	echo -e "  \t\t  The server will be restarted automatically when a file changes"
 	echo -e "  setup:\tSets up the virtualenv and installs a minium set of required plugins"
 	echo -e "  shell:\tStarts a new shell with the virtualenv activated"
+	echo -e "  test:\tRun unit testing"
 	echo -e "  uninstall:\tUninstall a plugin. Usage:"
 	echo -e "  \t\t  $0 uninstall [plugin-name]"
 	echo -e "  \t\t  where [plugin-name] is the name of the plugin."
@@ -120,6 +121,7 @@ setup() {
 		installPlugin $PLUGINPATH
 	done
 	# Setup git commit hooks
+	echo "---------------- Setup requirements"
 	pip install -U -r requirements.txt
 	captainhook install --use-virtualenv-python
 }
@@ -168,6 +170,9 @@ case $1 in
 		echo "Starting sandboxed shell"
 		PS1='(TellStick) \[\033[01;32m\]\u@\h\[\033[01;34m\] \$\[\033[00m\] '
 		sh
+	;;
+	test)
+		python -m unittest tests
 	;;
 	uninstall)
 		pip uninstall $2
