35b5cb569287c52d16e827ce6478d02ca897902f
Micke Prag <micke.prag@telldus.se>
2018-10-24 10:48:03 +0200

    Make upgrade plugin into package

--- upgrade/setup.py
+++ upgrade/setup.py
@@ -6,11 +6,7 @@ from setuptools import setup, find_packages
 setup(
 	name='Upgrade',
 	version='0.1',
-	packages=find_packages('src'),
-	package_dir={'':'src'},
-	entry_points={ \
-		'telldus.startup': ['c = upgrade:UpgradeManager [cREQ]']
-	},
+	packages=['upgrade'],
 	extras_require={
 		'upgrade': ['Base>=0.1'],
 	}
