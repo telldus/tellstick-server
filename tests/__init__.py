49b146810bca3de4325353d8d4d891ed5507f92f
Stefan Persson <stefan.persson@telldus.se>
2018-11-07 10:41:12 +0100

    Add unittest for sensor messages to CI

--- tests/__init__.py
+++ tests/__init__.py
@@ -3,4 +3,5 @@
 # Enable once fixed that the server is not started when running. See #199
 # from scheduler.base.tests import SchedulerTest
 
+from telldus.tests import TelldusTest
 from upgrade.tests import HotFixManagerTest
