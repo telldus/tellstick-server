b35d73c3dfc9e698679da0ce87a0bd19669f72f4
Micke Prag <micke.prag@telldus.se>
2019-11-12 14:00:28 +0100

    Do not fail if tests cannot be imported for some reason

--- tests/__init__.py
+++ tests/__init__.py
@@ -3,5 +3,14 @@
 # Enable once fixed that the server is not started when running. See #199
 # from scheduler.base.tests import SchedulerTest
 
-from telldus.tests import TelldusTest
-from upgrade.tests import HotFixManagerTest
+import logging
+
+try:
+	from telldus.tests import TelldusTest
+except ImportError:
+	logging.error('Could not import tests from plugin: telldus')
+
+try:
+	from upgrade.tests import HotFixManagerTest
+except ImportError:
+	logging.error('Could not import tests from plugin: upgrade')
